262dc358dcc6ed6b8bab9479fdbe7f0d97b68a67
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2023-07-20 08:53:45 +0300

    Add files via upload

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,21 +1,11 @@
-# Client configuration file (w/ Puppet)
-# Your SSH client configuration must be configured to use
-# the private key ~/.ssh/school
-# Your SSH client configuration must be configured to refuse
-# to authenticate using a password
-include stdlib
-file_line { 'NO password':
-    ensure             => present,
-    path               => '/etc/ssh/ssh_config',
-    line               => '    PasswordAuthentication no',
-    match              => '^PasswordAuthentication',
-    append_on_no_match => true,
-  }
+# Configure the ssh_config file to allow access to my ubuntu server with out requesting for a private key
 
-file_line { 'New private key holder':
-    ensure             => present,
-    path               => '/etc/ssh/ssh_config',
-    line               => '    IdentityFile ~/.ssh/school',
-    match              => '^IdentityFile',
-    append_on_no_match => true,
-  }
+file_line { 'Change the main private key':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+}
+
+file_line { 'No Authenticate with password':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
